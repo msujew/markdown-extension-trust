6548aeb656e58a59db87adc2844e711f2a274e23
Mark Sujew <mark.sujew@typefox.io>
2023-01-17 16:32:25 +0100

    Initial commit

---
+++ out/extension.js
@@ -0,0 +1,50 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "markdown-trust" is now active!');
+    vscode.languages.registerHoverProvider('*', {
+        provideHover() {
+            const md = new vscode.MarkdownString(`[trusted command](command:markdown-trust.trusted) and [untrusted command](command:markdown-trust.untrusted)`);
+            md.isTrusted = {
+                enabledCommands: ['markdown-trust.trusted']
+            };
+            return {
+                contents: [md]
+            };
+        },
+    });
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    vscode.commands.registerCommand('markdown-trust.trusted', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Trusted command!');
+    });
+    vscode.commands.registerCommand('markdown-trust.untrusted', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Untrusted command!');
+    });
+    vscode.commands.registerCommand('markdown-trust.webview', () => {
+        // Create and show panel
+        const panel = vscode.window.createWebviewPanel('webviewTrust', 'Webview Trust', vscode.ViewColumn.One, {
+            enableCommandUris: ['markdown-trust.trusted']
+        });
+        // And set its HTML content
+        panel.webview.html = `<a href="command:markdown-trust.trusted">trusted command</a> and <a href="command:markdown-trust.untrusted">untrusted command</a>`;
+    });
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
